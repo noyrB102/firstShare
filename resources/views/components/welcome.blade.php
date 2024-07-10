47bf298126554081f8f3cab953e53a7d574611cb
Bryon Swanson <bswanson@outlook.com>
2024-07-10 10:56:46 -0600

    Some possible accidental changes on Welcome

--- resources/views/components/welcome.blade.php
+++ resources/views/components/welcome.blade.php
@@ -2,10 +2,10 @@
     <x-application-logo class="block h-12 w-auto" />
 
     <h1 class="mt-8 text-2xl font-medium text-gray-900">
-        Welcome to your Jetstream application! This was pushed from GitHub!!!!
+        Welcome to your Jetstream application! This was pushed from
     </h1>
 
-    <p class="mt-6 text-gray-500 leading-regit alaxed">
+    <p class="mt-6 text-gray-500 leading-relaxed">
         Laravel Jetstream provides a beautiful, robust starting point for your next Laravel application. Laravel is designed
         to help you build your application using a development environment that is simple, powerful, and enjoyable. We believe
         you should love expressing your creativity through programming, so we have spent time carefully crafting the Laravel
