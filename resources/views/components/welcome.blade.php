c2a8d590755ca0c120ed2a13ec6b6d19a9444dbf
Bryon Swanson <bswanson@outlook.com>
2024-07-10 11:00:15 -0600

    Bg-blue test

--- resources/views/components/welcome.blade.php
+++ resources/views/components/welcome.blade.php
@@ -2,8 +2,9 @@
     <x-application-logo class="block h-12 w-auto" />
 
     <h1 class="mt-8 text-2xl font-medium text-gray-900">
-        Welcome to your Jetstream application! This was pushed from
+        Welcome to your Jetstream application! This was pushed from GitHub!!!
     </h1>
+    <p class="bg-blue-100">Did this update?</p>
 
     <p class="mt-6 text-gray-500 leading-relaxed">
         Laravel Jetstream provides a beautiful, robust starting point for your next Laravel application. Laravel is designed
