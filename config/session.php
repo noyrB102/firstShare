ff9a15dc0e397f599e9e6bc4a173f30ddcaed46e
Bryon Swanson <bswanson@outlook.com>
2024-07-11 07:06:35 -0600

    secure to true

--- config/session.php
+++ config/session.php
@@ -128,8 +128,7 @@ return [
     */
 
     'cookie' => env(
-        // 'SESSION_COOKIE',
-        'SESSION_SECURE_COOKIE', true ,
+        'SESSION_COOKIE',
         Str::slug(env('APP_NAME', 'laravel'), '_').'_session'
     ),
 
